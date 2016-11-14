0b33b109310084f0a8b49ba9216b1b053d866c1e
mirrayees <mirrayees859@gmail.com>
2016-11-15 02:12:51 +0530

    second

--- app.js
+++ app.js
@@ -5,14 +5,16 @@ var logger = require('morgan');
 var path = require('path');
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 
+var craw = require('./lib/crawler');
+var crawler = new craw("https://medium.com/", true);
+crawler.init();
 
-
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 module.exports = app;
